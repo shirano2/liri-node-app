e1f45cc9061e8ddb693e0682652e44499e8dd791
shirano2 <shirano2@naver.com>
2019-03-27 23:18:41 -0500

    finished

--- liri.js
+++ liri.js
@@ -8,19 +8,31 @@ var fs=require("fs");
 var moment=require("moment");
 
 var checkString=process.argv[2];
-var keyword=process.argv.splice(3).join("+");
+var keyword=process.argv.slice(3).join("+");
+
+var logMessage=new Date().toString();
+console.log(logMessage);
+
+function log() {
+  fs.appendFile("log.txt", logMessage, function(err) {
+    if (err) {
+      console.log("log recording failed: "+err);
+    }
+  });  
+}
 
 function concert(){
+    keyword=process.argv.slice(3).join("+");
+    logMessage=logMessage+", command : concert-this, keyword : "+keyword+";";
     axios.get("https://rest.bandsintown.com/artists/" + keyword + "/events?app_id=codingbootcamp").then(function(response) {
-        //console.log(response.data);
         for(var i=0; i<response.data.length;i++) {
-            console.log("============================================================================================================")
-            console.log("Num. "+(i+1))
-            console.log(response.data[i].venue.name);
-            console.log(response.data[i].venue.city+" in "+response.data[i].venue.country);
-            console.log(moment(response.data[i].venue.datetime).format("MM/DD/YYYY"));
+            console.log("============================================================================================================");
+            console.log("Name of the venue: "+response.data[i].venue.name);
+            console.log("Venue location: "+response.data[i].venue.city+" in "+response.data[i].venue.country);
+            console.log("Date of the Event: "+moment(response.data[i].venue.datetime).format("MM/DD/YYYY"));
             console.log("");
         }
+        console.log("============================================================================================================");
 	  })
 	  .catch(function(error) {
 	    if (error.response) {
@@ -30,16 +42,47 @@ function concert(){
 	    } else if (error.request) {
 	      console.log(error.request);
 	    } else {
-	      console.log("Error", error.message);
+	      console.log("There are no concerts :", error.message);
 	    }
 	    console.log(error.config);
 	  });
+}
+
+function spoti() {
+  keyword=keyword.split("+").join(" ");
+  keyword=keyword.split('"').join("");
+  if(keyword=="") {
+    keyword="The Sign";
+  } 
+  logMessage=logMessage+", command : spotify-this-song, keyword : "+keyword+";";
+  var song = {
+    "type": "track", 
+    "query": keyword
+  }
+  spotify.search(song, function(err, data) {
+    if (err) {
+      return console.log('There are no songs: ' + err);
+    }
+    for(var i=0; i<data.tracks.items.length;i++) {
+      if(data.tracks.items[i].name.trim().toLowerCase()==keyword.toLowerCase()) {
+        console.log("============================================================================================================");
+        console.log("Artist(s): "+data.tracks.items[i].artists[0].name);
+        console.log("The song's name: "+data.tracks.items[i].name);
+        if(data.tracks.items[i].preview_url!=null) {
+          console.log("A preview link of the song from Spotify: "+data.tracks.items[i].preview_url);
+        }
+        console.log("The album that the song is from: "+data.tracks.items[i].album.name)
+      }
+    }
+    console.log("============================================================================================================");
+  });
 }
 
 function movie() {
     if(keyword=="") {
         keyword="Mr.Nobody";
     }
+    logMessage=logMessage+", command : movie-this, keyword : "+keyword+";";
     axios.get("http://www.omdbapi.com/?t=" + keyword + "&y=&plot=short&apikey=trilogy").then(function(response) {
         // console.log(response.data);
         console.log("Title of the movie: " +response.data.Title);
@@ -56,60 +99,44 @@ function movie() {
         console.log("Actors in the movie: " +response.data.Actors);
       })
       .catch(function(error) {
-	    if (error.response) {
-	      console.log(error.response.data);
-	      console.log(error.response.status);
-	      console.log(error.response.headers);
-	    } else if (error.request) {
-	      console.log(error.request);
-	    } else {
-	      console.log("Error", error.message);
-	    }
-	    console.log(error.config);
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("There are no movies:", error.message);
+        }
+        console.log(error.config);
 	  });
+}
+
+function doWhat(){
+  fs.readFile("random.txt","utf8",function(error,data) {
+      if(error) {
+          return console.log(error);
+      }
+      checkString=data.split(",")[0];
+      keyword=data.split(",")[1];
+      main(); 
+  });
 }
 
 function main() {
     if(checkString=="concert-this") {
         concert();
     } else if (checkString=="spotify-this-song") {
-      keyword="The Sign";
-      spotify.search({ type: 'track', query: keyword }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-        for(var i=0; i<data.tracks.items.length;i++) {
-          if(data.tracks.items[i].name==keyword) {
-            console.log(data.tracks.items[i].artists[0].name);
-          }
-        }
-      })
+        spoti();
     } else if (checkString=="movie-this") {
         movie();
     } else if (checkString=="do-what-it-says") {
-        fs.readFile("random.txt","utf8",function(error,data) {
-            console.log(data);
-            if(error) {
-                return console.log(error);
-            }
-            checkString=data.split(",")[0];
-            keyword=data.split(",")[1];
-            main(); 
-        });
+        doWhat();
     } else {
         console.log("Error! There is no command!!");
         return;
     }
+    log();
 }
 
 main();
-
-
-// concert-this
-
-// spotify-this-song
-
-// movie-this
-
-// do-what-it-says
-// if()
