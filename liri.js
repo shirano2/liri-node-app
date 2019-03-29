78c0f660b1e6dba81bfc099180361043c1f37479
shirano2 <shirano2@naver.com>
2019-03-28 20:01:27 -0500

    log changed

--- liri.js
+++ liri.js
@@ -11,7 +11,6 @@ var checkString=process.argv[2];
 var keyword=process.argv.slice(3).join("+");
 
 var logMessage=new Date().toString();
-console.log(logMessage);
 
 function log() {
   fs.appendFile("log.txt", logMessage, function(err) {
@@ -31,8 +30,15 @@ function concert(){
             console.log("Venue location : "+response.data[i].venue.city+" in "+response.data[i].venue.country);
             console.log("Date of the Event : "+moment(response.data[i].venue.datetime).format("MM/DD/YYYY"));
             console.log("");
+            logMessage=logMessage+"============================================================================================================\r\n";
+            logMessage=logMessage+"Name of the venue : "+response.data[i].venue.name+"\r\n";
+            logMessage=logMessage+"Venue location : "+response.data[i].venue.city+" in "+response.data[i].venue.country+"\r\n";
+            logMessage=logMessage+"Date of the Event : "+moment(response.data[i].venue.datetime).format("MM/DD/YYYY")+"\r\n";
+            logMessage=logMessage+"\r\n";
         }
         console.log("============================================================================================================");
+        logMessage=logMessage+"============================================================================================================\r\n";
+        log();
 	  })
 	  .catch(function(error) {
 	    if (error.response) {
@@ -68,13 +74,21 @@ function spoti() {
         console.log("============================================================================================================");
         console.log("Artist(s) : "+data.tracks.items[i].artists[0].name);
         console.log("The song's name : "+data.tracks.items[i].name);
+        logMessage=logMessage+"============================================================================================================\r\n";
+        logMessage=logMessage+"Artist(s) : "+data.tracks.items[i].artists[0].name+"\r\n";
+        logMessage=logMessage+"The song's name : "+data.tracks.items[i].name+"\r\n";
+
         if(data.tracks.items[i].preview_url!=null) {
           console.log("A preview link of the song from Spotify : "+data.tracks.items[i].preview_url);
+          logMessage=logMessage+"A preview link of the song from Spotify : "+data.tracks.items[i].preview_url+"\r\n";
         }
         console.log("The album that the song is from : "+data.tracks.items[i].album.name)
+        logMessage=logMessage+"The album that the song is from : "+data.tracks.items[i].album.name+"\r\n";
       }
     }
     console.log("============================================================================================================");
+    logMessage=logMessage+"============================================================================================================\r\n";
+    log();
   });
 }
 
@@ -84,13 +98,18 @@ function movie() {
     }
     logMessage=logMessage+", command : movie-this, keyword : "+keyword+"\r\n";
     axios.get("http://www.omdbapi.com/?t="+keyword+"&y=&plot=short&apikey=trilogy").then(function(response) {
-      console.log("============================================================================================================");
+        console.log("============================================================================================================");
         console.log("Title of the movie : "+response.data.Title);
         console.log("Year the movie came out : "+response.data.Year);
         console.log("IMDB Rating of the movie : "+response.data.imdbRating);
+        logMessage=logMessage+"============================================================================================================\r\n";
+        logMessage=logMessage+"Title of the movie : "+response.data.Title+"\r\n";
+        logMessage=logMessage+"Year the movie came out : "+response.data.Year+"\r\n";
+        logMessage=logMessage+"IMDB Rating of the movie : "+response.data.imdbRating+"\r\n";
         for(var i=0; i<response.data.Ratings.length; i++) {
             if(response.data.Ratings[i].Source=="Rotten Tomatoes") {
                 console.log("Rotten Tomatoes Rating of the movie : "+response.data.Ratings[i].Value);
+                logMessage=logMessage+"Rotten Tomatoes Rating of the movie : "+response.data.Ratings[i].Value+"\r\n";
             }
         }
         console.log("Country where the movie was produced : "+response.data.Country);
@@ -98,6 +117,12 @@ function movie() {
         console.log("Plot of the movie : "+response.data.Plot);
         console.log("Actors in the movie : "+response.data.Actors);
         console.log("============================================================================================================");
+        logMessage=logMessage+"Country where the movie was produced : "+response.data.Country+"\r\n";
+        logMessage=logMessage+"Language of the movie : "+response.data.Language+"\r\n";
+        logMessage=logMessage+"Plot of the movie : "+response.data.Plot+"\r\n";
+        logMessage=logMessage+"Actors in the movie : "+response.data.Actors+"\r\n";
+        logMessage=logMessage+"============================================================================================================\r\n";
+        log();
       })
       .catch(function(error) {
         if (error.response) {
@@ -137,7 +162,6 @@ function main() {
         console.log("Error! There is no command!!");
         return;
     }
-    log();
 }
 
 main();
